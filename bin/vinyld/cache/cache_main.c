bcfe8ef3780fd90b3d47bbc010bf18883f3f904c
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2023-07-11 07:44:10 +0200

    cache: Ignore SIGUSR1
    Upstream: https://code.vinyl-cache.org/vinyl-cache/vinyl-cache/commit/5f5adbe21609ac20a6e46d4d64282c9b19f5ac61
    Port: exact

--- bin/vinyld/cache/cache_main.c
+++ bin/vinyld/cache/cache_main.c
@@ -392,6 +392,13 @@ cli_quit(int sig)
 	WRONG("It's time for the big quit");
 }
 
+static void
+child_sig_nore(int sig)
+{
+
+	(void)sig;
+}
+
 /*=====================================================================
  * XXX Generalize?
  */
@@ -479,6 +486,7 @@ child_main(int sigmagic, size_t altstksz)
 	(void)signal(SIGINT, SIG_DFL);
 	(void)signal(SIGTERM, SIG_DFL);
 	(void)signal(SIGQUIT, cli_quit);
+	(void)signal(SIGUSR1, child_sig_nore);
 
 #if defined(__FreeBSD__) && __FreeBSD_version >= 1000000
 	malloc_message = child_malloc_fail;
