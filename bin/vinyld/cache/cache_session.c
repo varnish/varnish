5aaf243ac65acdbb6db4af4a44d554fd9ce9b6fa
Asad Sajjad Ahmed <asadsa@varnish-software.com>
2025-05-21 15:27:27 +0200

    sess_close: restrict the range of stream_close_t
    For backends not all stream_close_t values make sense.
    Therefore, do not allow these to be specified as `doclose` attribute for
    failed backend transaction. No change is needed for existing errors.
    
    We should ideally split them up, but this is not possible as some
    functions are used by both sessions and fetches.
    
    One example of such function:
    - stream_close_t http_DoConnection(struct http *hp, stream_close_t sc_close);
    
    This patch is best viewed with --ignore-all-space
    
    Signed-off-by: Asad Sajjad Ahmed <asadsa@varnish-software.com>
    
    Upstream: https://code.vinyl-cache.org/vinyl-cache/vinyl-cache/commit/8087f08f3a789f6a32bdfaae02ec77a0391431cd
    Part of: https://code.vinyl-cache.org/vinyl-cache/vinyl-cache/pulls/4337
    
    Co-authored-by: Asad Sajjad Ahmed <asadsa@varnish-software.com>

--- bin/vinyld/cache/cache_session.c
+++ bin/vinyld/cache/cache_session.c
@@ -64,7 +64,7 @@ static const struct {
 
 enum sess_close {
 	SCE_NULL = 0,
-#define SESS_CLOSE(nm, stat, err, desc) SCE_##nm,
+#define SESS_CLOSE_C(nm, stat, err, desc) SCE_##nm,
 #include "tbl/sess_close.h"
 	SCE_MAX,
 };
@@ -77,7 +77,7 @@ const struct stream_close SC_NULL[1] = {{
 	.desc = "Not Closing",
 }};
 
-#define SESS_CLOSE(nm, stat, err, text) \
+#define SESS_CLOSE_C(nm, stat, err, text) \
 	const struct stream_close SC_##nm[1] = {{ \
 		.magic = STREAM_CLOSE_MAGIC, \
 		.idx = SCE_##nm, \
@@ -89,7 +89,7 @@ const struct stream_close SC_NULL[1] = {{
 
 static const stream_close_t sc_lookup[SCE_MAX] = {
 	[SCE_NULL] = SC_NULL,
-#define SESS_CLOSE(nm, stat, err, desc) \
+#define SESS_CLOSE_C(nm, stat, err, desc) \
 	[SCE_##nm] = SC_##nm,
 #include "tbl/sess_close.h"
 };
@@ -608,7 +608,7 @@ ses_close_acct(stream_close_t reason)
 
 	CHECK_OBJ_NOTNULL(reason, STREAM_CLOSE_MAGIC);
 	switch (reason->idx) {
-#define SESS_CLOSE(reason, stat, err, desc)		\
+#define SESS_CLOSE_C(reason, stat, err, desc)		\
 	case SCE_ ## reason:				\
 		VSC_C_main->sc_ ## stat++;		\
 		break;
