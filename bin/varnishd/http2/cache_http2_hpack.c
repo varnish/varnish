4b3c6ce59ab2782694e370219e8579a4ee255742
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2025-09-18 17:23:17 +0200

    http2_hpack: Check pseudo-header names with Tstreq()
    The problem with Tstrcmp() is that it would accept a prefix of the
    pseudo-header name.

--- bin/varnishd/http2/cache_http2_hpack.c
+++ bin/varnishd/http2/cache_http2_hpack.c
@@ -166,7 +166,7 @@ h2h_addhdr(struct http *hp, struct h2h_decode *d)
 
 	/* Match H/2 pseudo headers */
 	/* XXX: Should probably have some include tbl for pseudo-headers */
-	if (!Tstrcmp(nm, ":method")) {
+	if (Tstreq(nm, ":method")) {
 		hdr.b = val.b;
 		n = HTTP_HDR_METHOD;
 		disallow_empty = 1;
@@ -176,13 +176,13 @@ h2h_addhdr(struct http *hp, struct h2h_decode *d)
 			if (!vct_istchar(*p))
 				return (H2SE_PROTOCOL_ERROR);
 		}
-	} else if (!Tstrcmp(nm, ":path")) {
+	} else if (Tstreq(nm, ":path")) {
 		hdr.b = val.b;
 		n = HTTP_HDR_URL;
 		disallow_empty = 1;
 
 		// rfc9113,l,2693,2705
-		if (Tlen(val) > 0 && val.b[0] != '/' && Tstrcmp(val, "*")) {
+		if (Tlen(val) > 0 && val.b[0] != '/' && !Tstreq(val, "*")) {
 			VSLb(hp->vsl, SLT_BogoHeader,
 			    "Illegal :path pseudo-header %.*s",
 			    (int)Tlen(val), val.b);
@@ -194,7 +194,7 @@ h2h_addhdr(struct http *hp, struct h2h_decode *d)
 			if (vct_islws(*p) || vct_isctl(*p))
 				return (H2SE_PROTOCOL_ERROR);
 		}
-	} else if (!Tstrcmp(nm, ":scheme")) {
+	} else if (Tstreq(nm, ":scheme")) {
 		/* XXX: What to do about this one? (typically
 		   "http" or "https"). For now set it as a normal
 		   header, stripping the first ':'. */
@@ -208,7 +208,7 @@ h2h_addhdr(struct http *hp, struct h2h_decode *d)
 			if (!vct_istchar(*p))
 				return (H2SE_PROTOCOL_ERROR);
 		}
-	} else if (!Tstrcmp(nm, ":authority")) {
+	} else if (Tstreq(nm, ":authority")) {
 		/* NB: we inject "host" in place of "rity" for
 		 * the ":authority" pseudo-header.
 		 */
