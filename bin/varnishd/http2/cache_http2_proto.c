e804531e9ac7fdd55ca3a6319690f54471b3bdd1
Nils Goroll <nils.goroll@uplex.de>
2026-03-05 19:28:05 +0100

    Set wkm for http/2
    Luckily spotted by accident while working on #4467
    
    Ref 2ca1d4f1fe2e19ee66f8d477772dffe27f8d9dea

--- bin/varnishd/http2/cache_http2_proto.c
+++ bin/varnishd/http2/cache_http2_proto.c
@@ -780,6 +780,8 @@ h2_end_headers(struct worker *wrk, struct h2_sess *h2,
 		return (H2SE_PROTOCOL_ERROR); //rfc7540,l,3087,3090
 	}
 
+	http_SetWellKnownMethod(req->http);
+
 	if (req->http->hd[HTTP_HDR_URL].b == NULL) {
 		VSLb(h2->vsl, SLT_Debug, "Missing :path");
 		return (H2SE_PROTOCOL_ERROR); //rfc7540,l,3087,3090
@@ -788,8 +790,7 @@ h2_end_headers(struct worker *wrk, struct h2_sess *h2,
 	AN(req->http->hd[HTTP_HDR_PROTO].b);
 
 	if (*req->http->hd[HTTP_HDR_URL].b == '*' &&
-	    (Tlen(req->http->hd[HTTP_HDR_METHOD]) != 7 ||
-	    strncmp(req->http->hd[HTTP_HDR_METHOD].b, "OPTIONS", 7))) {
+	    ! http_method_eq(req->http->wkm, WKM_OPTIONS)) {
 		VSLb(h2->vsl, SLT_BogoHeader, "Illegal :path pseudo-header");
 		return (H2SE_PROTOCOL_ERROR); //rfc7540,l,3068,3071
 	}
