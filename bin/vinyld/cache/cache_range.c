f9d860a7811b65528f11764132f7e5134e4c9a88
Nils Goroll <nils.goroll@uplex.de>
2026-05-26 14:18:05 +0200

    cache_range: Avoid duplicate Content-Range headers
    (cherry picked from commit 2985f53763f1e4945d26fac5a13a6d2b9d929a21)
    
    Upstream: https://code.vinyl-cache.org/vinyl-cache/vinyl-cache/commit/2985f5376
    Port: exact

--- bin/vinyld/cache/cache_range.c
+++ bin/vinyld/cache/cache_range.c
@@ -116,6 +116,7 @@ vrg_dorange(struct req *req, intmax_t *clen, void **priv)
 	if (low < 0 || high < 0)
 		return (NULL);		// Allow 200 response
 
+	http_Unset(req->resp, H_Content_Range);
 	if (*clen >= 0) {
 		http_PrintfHeader(req->resp, "Content-Range: bytes %jd-%jd/%jd",
 		    (intmax_t)low, (intmax_t)high, *clen);
@@ -223,6 +224,7 @@ vrg_range_init(VRT_CTX, struct vdp_ctx *vdc, void **priv)
 		return (*priv == NULL ? 1 : 0);
 
 	VSLb(vdc->vsl, SLT_Debug, "RANGE_FAIL %s", err);
+	http_Unset(ctx->req->resp, H_Content_Range);
 	if (*vdc->clen >= 0)
 		http_PrintfHeader(ctx->req->resp,
 		    "Content-Range: bytes */%jd", *vdc->clen);
