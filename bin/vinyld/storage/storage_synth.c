3ff1c101b968b8e099a8db2ec629d7d846c042fc
Nils Goroll <nils.goroll@uplex.de>
2026-07-02 14:59:24 +0200

    Overlooked a viov_lease_t change
    Upstream: https://code.vinyl-cache.org/vinyl-cache/vinyl-cache/commit/613175cdffb600f823faf88ae7c4e31a39a39901
    Port: exact

--- bin/vinyld/storage/storage_synth.c
+++ bin/vinyld/storage/storage_synth.c
@@ -221,7 +221,7 @@ static void v_matchproto_(vai_return_f)
 ssy_ai_return(struct worker *wrk, vai_hdl vhdl, struct vscaret *scaret)
 {
 	struct ssy_hdl *hdl;
-	uintptr_t *p;
+	viov_lease_t *p;
 
 	(void) wrk;
 	CAST_VAI_HDL_NOTNULL(hdl, vhdl, SSY_HDL_MAGIC);
