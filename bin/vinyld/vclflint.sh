436fd578fab0e42a99ecb32bb811d9f63bbc4efa
Nils Goroll <nils.goroll@uplex.de>
2026-08-27 14:53:45 +0200

    Polish vclflint.sh
    fix a typo (used sh file as vcl) and make picky dash happy
    
    Upstream: https://code.vinyl-cache.org/vinyl-cache/vinyl-cache/commit/d405c7de12aefa13901a402eb319c1aae67641fd
    Port: adjusted -- vinyld binary renamed to varnishd

--- bin/vinyld/vclflint.sh
+++ bin/vinyld/vclflint.sh
@@ -4,20 +4,20 @@ set -e
 
 #
 # Run flexelint on the VCL output
-LIBS="-p vmod_path=$PWD/../../vmod/.libs"
+VMODS="$PWD/../../vmod/.libs"
 
-if [ "x$1" = "x" ] ; then
-	if ! ./varnishd $LIBS -C -f $PWD/vclflint.sh 2> /tmp/_.c ; then
-		cat >&2 /tmp/_.c
+if test "x$1" = "x" ; then
+	if ! ./varnishd -p "vmod_path=$VMODS" -C -f "$PWD/vclflint.vcl" 2>"/tmp/_.c" ; then
+		cat >&2 "/tmp/_.c"
 		exit 1
 	fi
-elif [ -f $1 ] ; then
-	if ! ./varnishd $LIBS -C -f $1 2> /tmp/_.c ; then
-		cat >&2 /tmp/_.c
+elif test -f "$1" ; then
+	if ! ./varnishd -p "vmod_path=$VMODS" -C -f "$1" 2>"/tmp/_.c" ; then
+		cat >&2 "/tmp/_.c"
 		exit 1
 	fi
 else
 	echo "usage!" 1>&2
 fi
 
-flexelint vclflint.lnt /tmp/_.c
+flexelint vclflint.lnt "/tmp/_.c"
