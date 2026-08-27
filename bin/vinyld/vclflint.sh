e0e8c9eca09017d2170fceb4c6586af1a0efd240
Nils Goroll <nils.goroll@uplex.de>
2026-08-27 11:11:38 +0200

    Make vclflint more useful
    - adjust LIBS so it should be correct for not phk
    - output vinyld error when it fails
    - vinyld outputs C on stderr
    - add a "coverage" vcl to avoid flint reporting all kinds of unused features
    - clean up and add to silencing rules
    
    Upstream: https://code.vinyl-cache.org/vinyl-cache/vinyl-cache/commit/b86278cd4bb576140268a504896f6eafd5bc5957
    Port: adjusted -- vinyld binary renamed to varnishd in vclflint.sh

--- bin/vinyld/vclflint.sh
+++ bin/vinyld/vclflint.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
+
+set -e
+
 #
 # Run flexelint on the VCL output
-LIBS="-p vmod_path=/home/phk/Varnish/trunk/varnish-cache/vmod/.libs"
+LIBS="-p vmod_path=$PWD/../../vmod/.libs"
 
 if [ "x$1" = "x" ] ; then
-	./varnishd $LIBS -C -b localhost > /tmp/_.c
+	if ! ./varnishd $LIBS -C -f $PWD/vclflint.sh 2> /tmp/_.c ; then
+		cat >&2 /tmp/_.c
+		exit 1
+	fi
 elif [ -f $1 ] ; then
-	./varnishd $LIBS -C -f $1 > /tmp/_.c
+	if ! ./varnishd $LIBS -C -f $1 2> /tmp/_.c ; then
+		cat >&2 /tmp/_.c
+		exit 1
+	fi
 else
 	echo "usage!" 1>&2
 fi
